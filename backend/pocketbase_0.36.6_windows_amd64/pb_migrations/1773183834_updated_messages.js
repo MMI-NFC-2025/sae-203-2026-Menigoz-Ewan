b4d1de52d471bcba1016b8e87a2960be135df8ca
Menigoz-Ewan <ewan.menigoz@edu.univ-fcomte.fr>
2026-03-11 01:35:11 +0100

    étape 4 (tailwind) terminée

---
+++ backend/pocketbase_0.36.6_windows_amd64/pb_migrations/1773183834_updated_messages.js
@@ -0,0 +1,83 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2605467279")
+
+  // add field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1819170229",
+    "max": 0,
+    "min": 0,
+    "name": "nom",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "exceptDomains": null,
+    "hidden": false,
+    "id": "email3885137012",
+    "name": "email",
+    "onlyDomains": null,
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "email"
+  }))
+
+  // add field
+  collection.fields.addAt(3, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text773020061",
+    "max": 0,
+    "min": 0,
+    "name": "sujet",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(4, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3065852031",
+    "max": 0,
+    "min": 0,
+    "name": "message",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2605467279")
+
+  // remove field
+  collection.fields.removeById("text1819170229")
+
+  // remove field
+  collection.fields.removeById("email3885137012")
+
+  // remove field
+  collection.fields.removeById("text773020061")
+
+  // remove field
+  collection.fields.removeById("text3065852031")
+
+  return app.save(collection)
+})
